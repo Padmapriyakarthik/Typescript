2f1d4f84c0cad9c9d2d1fbe8c6a542d400f0850e
Padmapriyakarthik <71600824+Padmapriyakarthik@users.noreply.github.com>
2021-02-09 19:05:06 +0530

    Add files via upload

---
+++ task1/index.js
@@ -0,0 +1,15 @@
+// Leap Year or Not
+var leap = function (year) {
+    return (year % 2 == 0) ? true : false;
+};
+console.log(leap(1998));
+console.log(leap(2021));
+//String Reverse
+var reverse = function (word) {
+    var reversed_word = "";
+    for (var i = word.length - 1; i >= 0; i--) {
+        reversed_word += word[i];
+    }
+    return reversed_word;
+};
+console.log("The reversed word of padmapriya is" + reverse('padmapriya'));
---
+++ task1/index.ts
@@ -0,0 +1,21 @@
+
+// Leap Year or Not
+let leap=(year:number):boolean=> {
+    return (year%2==0)? true : false;
+}
+
+console.log(leap(1998));
+console.log(leap(2021));
+
+//String Reverse
+
+let reverse=(word:string):string=>{
+    let reversed_word=""
+    for(let i=word.length-1;i>=0;i--)
+    {
+        reversed_word+=word[i];
+    }
+    return reversed_word;
+}
+
+console.log("The reversed word of padmapriya is"+reverse('padmapriya'));
